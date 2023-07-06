32579eb5dc4b8abfb904ecb01025bf5ddcec07ef
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-06 11:13:07 +0900

    [G] Pass len test
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- SimJudge/SimJudge.cpp
+++ SimJudge/SimJudge.cpp
@@ -1,5 +1,19 @@
+#include <xstring>
+
 class SimJudge
 {
 public:
-
+	int calPointLength(const std::string& a, const std::string& b)
+	{
+		if (a.length() == b.length())
+			return 60;
+		if (a.length() > b.length())
+		{
+			return (double)(a.length() - b.length()) / a.length() * 60;
+		} else
+		{
+			return (double)(b.length() - a.length()) / b.length() * 60;
+		}
+		return 0;
+	}
 };
--- SimJudge_test/test.cpp
+++ SimJudge_test/test.cpp
@@ -1,6 +1,38 @@
 #include "pch.h"
+#include "..\SimJudge\SimJudge.cpp"
+#include <string>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+TEST(TestSimJudge, TestSameLen) {
+	SimJudge judge{};
+	string a = "abcd";
+	string b = "efgh";
+
+	int expected = 60;
+	int actual = judge.calPointLength(a, b);
+
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TestSimJudge, TestA2B4) {
+	SimJudge judge{};
+	string a = "ab";
+	string b = "defg";
+
+	int expected = 30;
+	int actual = judge.calPointLength(a, b);
+
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(TestSimJudge, TestA4B2) {
+	SimJudge judge{};
+	string a = "defg";
+	string b = "ab";
+
+	int expected = 30;
+	int actual = judge.calPointLength(a, b);
+
+	EXPECT_EQ(expected, actual);
 }
