71248494fc0a638ab47c0b4579f4a3c3a49a28f9
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-06 11:32:01 +0900

    [F] Modi calc Point function
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- SimJudge/SimJudge.cpp
+++ SimJudge/SimJudge.cpp
@@ -5,15 +5,20 @@ class SimJudge
 public:
 	int calPointLength(const std::string& a, const std::string& b)
 	{
-		if (a.length() == b.length())
-			return 60;
-		if (a.length() > b.length())
-		{
-			return (double)(a.length() - b.length()) / a.length() * 60;
-		} else
-		{
-			return (double)(b.length() - a.length()) / b.length() * 60;
-		}
-		return 0;
+		int a_len = a.length();
+		int b_len = b.length();
+
+		if (a_len == b_len)
+			return LENGTH_JUDGE_MAX_POINT;
+		return calPointPartial(a_len, b_len);
 	}
+
+	int calPointPartial(int a_len, int b_len)
+	{
+		if (a_len > b_len)
+			return int((double)(a_len - b_len) / a_len * LENGTH_JUDGE_MAX_POINT);
+		return int((double)(b_len - a_len) / b_len * LENGTH_JUDGE_MAX_POINT);
+	}
+
+	const int LENGTH_JUDGE_MAX_POINT = 60;
 };
