b75bd53cabb0dfbebd5af227431c0f81adec675a
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-06 13:18:40 +0900

    [F] Make function for double check
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- SimJudge/SimJudge.cpp
+++ SimJudge/SimJudge.cpp
@@ -3,15 +3,20 @@
 class SimJudge
 {
 public:
+	bool isDoubleDiff(int a_len, int b_len)
+	{
+		return a_len >= b_len * 2 || a_len * 2 <= b_len;
+	}
+
 	int calPointLength(const std::string& a, const std::string& b)
 	{
 		int a_len = a.length();
 		int b_len = b.length();
 
-		if (a_len >= b_len * 2 || a_len * 2 <= b_len)
-			return 0;
 		if (a_len == b_len)
 			return LENGTH_JUDGE_MAX_POINT;
+		if (isDoubleDiff(a_len, b_len))
+			return 0;
 		return calPointPartial(a_len, b_len);
 	}
 
