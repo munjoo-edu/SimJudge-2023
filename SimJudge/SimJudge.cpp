85bd72b9cb89ccee1eff15b0831418e34184f0aa
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-06 13:42:28 +0900

    [F] Remove bigger check logic on each function
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- SimJudge/SimJudge.cpp
+++ SimJudge/SimJudge.cpp
@@ -1,30 +1,37 @@
 #include <string>
+#include <algorithm>
+using namespace std;
 
 class SimJudge
 {
 public:
+	bool isSame(int a_len, int b_len)
+	{
+		return a_len == b_len;
+	}
+
 	int calPointLength(const std::string& a, const std::string& b)
 	{
-		int a_len = a.length();
-		int b_len = b.length();
+		int bigger = max(a.length(), b.length());
+		int smaller = min(a.length(), b.length());
 
-		if (a_len == b_len)
+		if (isSame(bigger, smaller))
 			return LENGTH_JUDGE_MAX_POINT;
-		if (isDoubleDiff(a_len, b_len))
+
+		if (isDoubleDiff(bigger, smaller))
 			return 0;
-		return calPointPartial(a_len, b_len);
+
+		return calPointPartial(bigger, smaller);
 	}
 
 	bool isDoubleDiff(int a_len, int b_len)
 	{
-		return a_len >= b_len * 2 || a_len * 2 <= b_len;
+		return a_len >= b_len * 2;
 	}
 
 	int calPointPartial(int a_len, int b_len)
 	{
-		if (a_len > b_len)
-			return int((double)(a_len - b_len) / b_len * LENGTH_JUDGE_MAX_POINT);
-		return int((double)(b_len - a_len) / a_len * LENGTH_JUDGE_MAX_POINT);
+		return int((double)(a_len - b_len) / b_len * LENGTH_JUDGE_MAX_POINT);
 	}
 
 	const int LENGTH_JUDGE_MAX_POINT = 60;
